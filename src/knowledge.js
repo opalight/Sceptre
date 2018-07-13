e31f2642e0cb7f0b81ae188b787617fce809207b
collinsnji <collins.geek@gmail.com>
2018-07-13 13:25:50 -0400

    Extend knowlege api. [Introduced a bug]

--- src/knowledge.js
+++ src/knowledge.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2018 Collin Grimm
+ * 
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
 const request = require('request');
 require('dotenv').config();
 
@@ -8,9 +15,9 @@ const Parser = (obj) => {
     for (let i in obj.itemListElement) {
         objData.push({
             name: obj.itemListElement[i].result.name,
-            description: obj.itemListElement[i].result.description
-        }
-        )
+            description: obj.itemListElement[i].result.description,
+            detail: obj.itemListElement[i].result.detailedDescription
+        })
     }
     return objData;
 }
@@ -20,19 +27,26 @@ class Knowledge {
             method: 'GET',
             url: 'https://kgsearch.googleapis.com/v1/entities:search',
             qs:
-                {
-                    query: searchQuery,
-                    limit: 5,
-                    indent: true,
-                    key: process.env.KG_SEARCH
-                }
+            {
+                query: searchQuery,
+                limit: 5,
+                indent: true,
+                key: process.env.KG_SEARCH
+            }
         };
         request(params, (err, res, body) => {
-            if (err) throw new Error(err.message);
+            if (err) return console.log(`Error (${err.status}): ${err.message}`);
+            
             const data = Parser(JSON.parse(body));
-            console.log(data);
+            console.log(`Here are some results for "${searchQuery}"\n`);
+            console.log(body.toString());
+            data.forEach(result => {
+                console.log(result, '\n\n\n');
+
+                console.log(`${result.name}\n${result.description}\n${result.detail.articleBody}\n\n`);
+            });
         });
     }
 }
 
-new Knowledge().Search('Meaning of life');
+new Knowledge().Search('Taylor Swift');
