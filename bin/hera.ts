2e89d79ac32ed5707a56f4081a9f6cd51b37533d
collinsnji <collins.geek@gmail.com>
2018-07-26 16:51:20 -0400

    Migrate binary file

--- bin/hera.ts
+++ bin/hera.ts
@@ -7,21 +7,22 @@
  * https://opensource.org/licenses/MIT
  */
 
-'use strict';
+/// <reference path="../hera.d.ts" />
 
-const inquirer = require('inquirer');
-const program = require('commander');
+import inquirer from 'inquirer';
+import program from 'commander';
 
-const hera = require('../index');
-const questions = require('../src/questions');
+import { Hera as hera } from '../index';
+import questions from '../src/questions';
+
 
 // Init
 const Hera = new hera();
 
 const Runner = {
-    lyrics: (song) => {
-        song = song.filter((el) => { return typeof el === 'string' })
-        let [songName, artist] = song.toString().split('-');
+    lyrics: (song: Array<any>) => {
+        song = song.filter((el: any) => { return typeof el === 'string' })
+        let [songName, artist]: Array<string> = song.toString().split('-');
 
         songName = songName.split(',').join('');
         artist = artist.split(',').join('');
@@ -29,24 +30,24 @@ const Runner = {
     },
     mail: () => {
         // Send Email
-        inquirer.prompt(questions.email).then(input => {
-            Hera.Mail(input.recipient, input.subject, input.message, input.password);
+        inquirer.prompt(questions.email).then((input: any) => {
+            Hera.Mail(input);
         });
     },
-    tasks: (command) => {
+    tasks: (command: string) => {
         switch (command.toString().trim()) {
             case 'list':
                 Hera.Tasks().showAllTask();
                 break;
             case 'new':
-                inquirer.prompt(questions.task).then(input => {
-                    Hera.Tasks().newTask(input.title, input.body);
+                inquirer.prompt(questions.tasks).then((input: any) => {
+                    Hera.Tasks().newTask(input);
                 });
                 break;
             // FIXME
             case 'update':
-                inquirer.prompt(questions.task).then(input => {
-                    Hera.Tasks().updateTask(input.title, input.body);
+                inquirer.prompt(questions.tasks).then((input: any) => {
+                    Hera.Tasks().updateTask({ title: input.title, body: input.body, id: input.id });
                 });
                 break;
             case 'purge':
@@ -60,13 +61,13 @@ const Runner = {
 program
     .command('lyrics')
     .alias('ly')
-    .action((...song) => { Runner.lyrics(song); });
+    .action((...song) => Runner.lyrics(song));
 program
     .command('mail')
-    .action(arg => { Runner.mail(arg); });
+    .action(() => Runner.mail());
 program
     .command('tasks')
     .alias('reminders')
-    .action(command => { Runner.tasks(command); })
+    .action(command => Runner.tasks(command))
 
 program.parse(process.argv);
